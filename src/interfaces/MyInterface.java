f894feb58127fc4f7b334a90177a31b4f288917a
Omid Mohebbi <omid.mohebbi@rdsysco.com>
2021-05-10 13:40:31 +0430

    Refactor project for developing examples.

--- src/interfaces/MyInterface.java
+++
@@ -1,7 +0,0 @@
-package interfaces;
-
-public interface MyInterface {
-    public static void printHelloWorld(){
-        System.out.println("Hello Wordl from static method in interface in java 8");
-    }
-}
--- src/nashorn/MainClass.java
+++
@@ -1,7 +0,0 @@
-package nashorn;
-
-public class MainClass {
-    public static void main(String[] args){
-        System.out.println();
-    }
-}
