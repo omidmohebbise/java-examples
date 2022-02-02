8d860198f8919c60dcfe73b1341d97f9f3cbbf5c
omid mohebbi <omidmohebbise@gmail.com>
2022-02-02 16:01:48 +0330

    First Parallel Practicing.

---
+++ java-parallel/src/main/java/com/omidmhebbi/parallel/Practice1.java
@@ -0,0 +1,24 @@
+package com.omidmhebbi.parallel;
+
+public class Practice1 {
+
+    public static void main(String[] args) {
+        long kilobyte = 1024;
+        long megabyte = kilobyte*1024;
+        long gigabyte = megabyte*1024;
+
+
+        Runtime runtime = Runtime.getRuntime();
+
+        //Total designated memory, this will equal the configured -Xmx value
+        System.out.println("Total Memory: " + runtime.maxMemory()/ megabyte + " mb");
+
+        //Total allocated memory, is the total allocated space reserved for the java process:
+        System.out.println("Total Memory: " + runtime.totalMemory()/ megabyte + " mb");
+        System.out.println("free memory: "  + runtime.freeMemory() / kilobyte + " kb   |   " +  + runtime.freeMemory() / megabyte + " Mb");
+
+
+        System.out.println("Process Id : " + ProcessHandle.current().pid());
+
+    }
+}
