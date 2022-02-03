a3f62b6211a942d7c42b40ab1cb60a039f0f780b
omid mohebbi <omidmohebbise@gmail.com>
2022-02-03 11:37:36 +0330

    Practicing on data racing in java multithreading

---
+++ java-parallel/src/main/java/com/omidmhebbi/parallel/basic/practice5/Practice5.java
@@ -0,0 +1,35 @@
+package com.omidmhebbi.parallel.basic.practice5;
+
+class MyThread extends Thread{
+    public MyThread(String name) {
+        super(name);
+    }
+    public static int counter = 0;
+    @Override
+    public void run() {
+        int i = 0 ;
+        //while (i++ < 10 ){    => in this case it is ok but in large number of accessing to the share resource the result is not the same per execution.
+        while (i++ < 10000000 ){
+            counter++;
+        }
+    }
+}
+
+public class Practice5 {
+
+    public static void main(String[] args) throws InterruptedException {
+        MyThread thread1 = new MyThread("First Thread");
+        MyThread thread2 = new MyThread("Second Thread");
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+
+        System.out.println("Counter = " + MyThread.counter);
+
+
+    }
+}
