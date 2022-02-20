dbe0a8cae1501f83da866e5a5977001d6c2a81f4
omid mohebbi <omidmohebbise@gmail.com>
2022-02-20 08:32:14 +0330

    Practice on Race Condition multi-thread programming.

---
+++ java-parallel/src/main/java/com/omidmhebbi/parallel/advanced/practice04/RaceCondition.java
@@ -0,0 +1,61 @@
+package com.omidmhebbi.parallel.advanced.practice04;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+class MyThread extends Thread {
+    public static int counter = 1;
+    public static Lock writeLock = new ReentrantLock();
+
+    public MyThread(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run() {
+        if (getName().contains("Doubler")) {
+            try {
+                writeLock.lock();
+                counter *= 2;
+                System.out.println(getName()+" => " + counter );
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                writeLock.unlock();
+            }
+        }else{
+            try {
+                writeLock.lock();
+                counter += 2;
+                System.out.println(getName()+" => " + counter );
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                writeLock.unlock();
+            }
+        }
+    }
+}
+
+public class RaceCondition {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            MyThread myThread1 = new MyThread("Doubler" + i);
+            MyThread myThread2 = new MyThread("Adder" + (i*2 + 1));
+            myThread1.start();
+            myThread2.start();
+            try {
+                myThread1.join();
+                myThread2.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
+
+        System.out.println(MyThread.counter);
+    }
+
+}
---
+++ java-parallel/src/main/java/com/omidmhebbi/parallel/advanced/practice04/ReadMe.md
@@ -0,0 +1,4 @@
+## Race Condition
+
+If run the application multiple times, the result would be different.
+Race condition is related to order of thread execution. 
