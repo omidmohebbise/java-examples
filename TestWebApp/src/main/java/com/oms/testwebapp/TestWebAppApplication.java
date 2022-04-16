c9a8364536dd0a2fab15934cc568fbd18763caf4
omid mohebbi <omidmohebbise@gmail.com>
2022-04-16 16:07:16 +0430

    Hands on java module

---
+++ TestWebApp/src/main/java/com/oms/testwebapp/TestWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.oms.testwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestWebAppApplication.class, args);
+    }
+
+}
