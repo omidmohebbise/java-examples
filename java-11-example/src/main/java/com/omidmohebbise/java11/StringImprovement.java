33b83a0ea69672b5174d333bea2858a14e5eca53
omid mohebbi <omidmohebbise@gmail.com>
2022-11-26 11:44:43 +0300

    Java 11 Project - working on String

---
+++ java-11-example/src/main/java/com/omidmohebbise/java11/StringImprovement.java
@@ -0,0 +1,48 @@
+package com.omidmohebbise.java11;
+
+import java.util.stream.Collectors;
+
+public class StringImprovement {
+    public static void main(String[] args) {
+        //blankMethod();
+        //lines();
+        //strips();
+        repeat();
+    }
+
+    public static void blankMethod() {
+        System.out.println(" ".isBlank()); //true
+
+        String s = "Anupam";
+        System.out.println(s.isBlank()); //false
+        String s1 = "";
+        System.out.println(s1.isBlank()); //true
+    }
+
+    public static void lines() {
+        String str = "JD\nJD\nJD";
+        System.out.println(str);
+        System.out.println(str.lines().collect(Collectors.toList()));
+    }
+
+    public static void strips() {
+        String str = " JD ";
+        System.out.print("Start");
+        System.out.print(str.strip());
+        System.out.println("End");
+
+
+        System.out.print("Start");
+        System.out.print(str.stripLeading());
+        System.out.println("End");
+
+        System.out.print("Start");
+        System.out.print(str.stripTrailing());
+        System.out.println("End");
+    }
+
+    public static void repeat(){
+        String str = "=".repeat(3);
+        System.out.println(str); //prints ===
+    }
+}
