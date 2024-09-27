9d4347a8b02f6d68af0fa3e4756ab2dd4594068f
omidmohebbi <omidmohebbise@gmail.com>
2024-09-27 16:58:24 +0200

    Add example 430-441 JEP

---
+++ java-21-examples/src/main/java/com/omidmohebbise/java21examples/Jep430StringTemplates.java
@@ -0,0 +1,20 @@
+package com.omidmohebbise.java21examples;
+
+
+public class Jep430StringTemplates {
+    public static void main(String[] args) {
+        String name = "Alice";
+        int age = 30;
+        // Using String Template with the STR prefix and ${} for expressions
+        //String greeting = STR."Hello, my name is ${name} and I am ${age} years old.";
+        //System.out.println(greeting);
+
+
+//        String query = STR."""
+//                SELECT * FROM users
+//                WHERE name = ${name}
+//                AND age > ${age}
+//                """;
+        //System.out.println(query);
+    }
+}
---
+++ java-21-examples/src/main/java/com/omidmohebbise/java21examples/Jep440RecordPatterns.java
@@ -0,0 +1,29 @@
+package com.omidmohebbise.java21examples;
+
+public class Jep440RecordPatterns {
+    public record Person(String name, int age) {}
+    public static void main(String[] args) {
+        Object obj = new Person("Alice", 30);
+
+
+        // Use pattern matching with record pattern in a switch statement
+        String result = switch (obj) {
+            case Person(String name, int age) ->
+                    "Name: " + name + ", Age: " + age; // Deconstruct the record into name and age
+            default -> "Unknown object";
+        };
+
+        System.out.println(result);
+
+
+        Object obj1 = "";
+
+        result = switch (obj1) {
+            case Person(String name, int age) ->
+                    "Name: " + name + ", Age: " + age; // Deconstruct the record into name and age
+            default -> "Unknown object";
+        };
+
+        System.out.println(result);
+    }
+}
